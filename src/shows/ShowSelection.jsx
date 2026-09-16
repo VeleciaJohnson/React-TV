e02f69f0fd01dcaa68c827196ba3703e0773af33
velecia <veleciajohnson@Ladys-MacBook-Air.local>
2026-09-16 18:31:39 -0400

    React-TV

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,22 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ shows, setSelectedShow }) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          key={show.name}
+          href="#"
+          className="show"
+          onClick={(event) => {
+            event.preventDefault();
+            setSelectedShow(show);
+          }}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
